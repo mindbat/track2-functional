672be7dab8d4a5afd0e59bffc0829e079f135b52
tcushman <ted@homespotter.com>
2015-06-22 18:57:31 -0500

    Updating versions on dependencies `compojure` and `ring/ring-defaults`

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
     :dependencies [[org.clojure/clojure "1.6.0"]
-                   [compojure "1.3.1"]
-                   [ring/ring-defaults "0.1.2"]
+                   [compojure "1.3.4"]
+                   [ring/ring-defaults "0.1.5"]
                    [ring-server "0.4.0"]
                    [hiccup "1.0.5"]
                    [clj-http "1.1.2"]
